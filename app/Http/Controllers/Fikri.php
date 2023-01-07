ebff39aa7609099606a02c4d7f31146850743393
latifulfikri <latifulfikri1902@gmail.com>
2023-01-07 17:11:18 +0700

    fix env

--- app/Http/Controllers/Fikri.php
+++ app/Http/Controllers/Fikri.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class Fikri extends Controller
 {
@@ -13,7 +14,15 @@ class Fikri extends Controller
      */
     public function index()
     {
-        //
+        // variabel data bakal diisi data dari model namanya product
+
+        // SELECT * FROM 'product'
+        $data = Product::get();
+
+        var_dump($data);
+
+        // manggil file view nya
+        return view('fikri.index');
     }
 
     /**
