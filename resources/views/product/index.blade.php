ede7d878b4e3ed82fb995d94b42e446637bba3f5
latifulfikri <latifulfikri1902@gmail.com>
2023-01-08 13:02:19 +0700

    producet - fexed index

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -7,7 +7,7 @@
 <!-- navigasi -->
 @section('breadcrumb')
 <li class="breadcrumb-item text-sm"><a href="" class="opacity-5 text-dark">Dashboard</a></li>
-<li class="breadcrumb-item text-sm text-dark active">Product</li>
+<li class="breadcrumb-item text-sm text-dark active">Home</li>
 @endsection
 
 <!-- isinya -->
@@ -23,61 +23,6 @@
                                 <h6>Products Table</h6>
                             </div>
                             <div class="col-lg-auto ms-auto">
-<<<<<<< HEAD
-                                <a href="" class="btn btn-success">Add Data</a>
-                            </div>
-                        </div>
-                    </div>
-                <div class="card-body px-0 pt-0 pb-2">
-                <div class="table-responsive p-0">
-                    <table class="table align-items-center mb-0">
-                        <thead>
-                            <tr>
-                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">ID</th>
-                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Name</th>
-                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Type</th>
-                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Description</th>
-                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Price</th>
-                                <th class="text-secondary opacity-7"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($products as $product)
-                            <tr>
-                                <td class="text-center">
-                                    {{ $loop->iteration }}
-                                </td>
-                                <td>
-                                    <div class="d-flex px-2 py-1">
-                                        <div>
-                                            <img src="{{ url('/cus_asset/images/') }}/{{ $product->type }}.png" class="avatar avatar-sm me-3" alt="user1">
-                                        </div>
-                                        <div class="d-flex flex-column justify-content-center">
-                                            <h6 class="mb-0 text-sm">{{ $product->name }}</h6>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class="text-center align-middle text-sm">
-                                    <span class="badge badge-sm bg-gradient-success">{{ $product->type }}</span>
-                                </td>
-                                <td class="align-middle">
-                                    <p class="text-xs font-weight-bold mb-0">{{ $product->description }}</p>
-                                </td>
-                                <td class="align-middle text-center">
-                                    <span class="text-secondary text-xs font-weight-bold">{{ $product->price }}</span>
-                                </td>
-                                <td class="align-middle">
-                                    <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
-                                    Edit
-                                    </a>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-                </div>
-=======
                                 <a href="{{ url('/dashboard/product/create') }}" class="btn btn-success">Add new product</a>
                             </div>
                         </div>
@@ -141,7 +86,6 @@
                             </table>
                         </div>
                     </div>
->>>>>>> bab540d843be278c9658d53fa14bde66196a4169
                 </div>
             </div>
         </div>
