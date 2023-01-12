180e9a4705f203238cb581e995f253226b392245
IanPutra <85535686+IanPutra@users.noreply.github.com>
2023-01-12 21:15:01 +0700

    huft

--- resources/views/customer_view/bookservice.blade.php
+++ resources/views/customer_view/bookservice.blade.php
@@ -1,56 +1,54 @@
 @extends('/customer_view.template')
 
 @section('main-content')
-<link href="{{ url('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
-<link href="{{ url('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
-<link href="{{ url('assets/css/nucleo-svg.css" rel="stylesheet') }}" />
+
     <div class="container">
         <div class="row">
-            <div class="col-md-7 py-5">
+            <div class="col-md-12 py-5">
                 <div class="titlepage">
                     <h1><b>Form for Your Device Reparation</b></h1>
                 </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-3">
-                <form action="#" method="post">
-                                @csrf
-                                {{-- kolom nama --}}
-                                <label class="mt-2 mb-" for="nameid">Customer Id</label>
-                                <input type="text" name="nameid" id="nameid" class="form-control @error('nameid') is-invalid @enderror" placeholder="name id" >
-                                @error('nameid')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                                @enderror
+                <div class="col-md-8 py-2">
+                    <form action="#" method="post">
+                            @csrf
+                            {{-- kolom nama --}}
+                            <label class="mt-1" for="nameid"><h2>Customer Id</h2></label>
+                            <input type="text" name="nameid" id="nameid" class="form-control @error('nameid') is-invalid @enderror" placeholder="name id" >
+                            @error('nameid')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
 
-                                {{-- kolom payment --}}
-                                <label class="mt-2" for="payment">Payment</label>
-                                <select name="payment" id="payment" class="form-select @error('payment') is-invalid @enderror">
-                                    <option value="">Select type</option>
-                                    <option value="QRIS">QRIS</option>
-                                    <option value="BCA">BCA</option>
-                                    <option value="Mandiri">Mandiri</option>
-                                </select>
-                                @error('payment')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                                @enderror
+                            {{-- kolom payment --}}
+                            <label class="mt-1" for="payment"><h2>Payment</h2></label>
+                            <select name="payment" id="payment" class="custom-select d-block w-100 @error('payment') is-invalid @enderror">
+                                <option value="">Select type</option>
+                                <option value="QRIS">QRIS</option>
+                                <option value="BCA">BCA</option>
+                                <option value="Mandiri">Mandiri</option>
+                            </select>
+                            @error('payment')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                            <p>
+                                
+                            </p>
+                            {{-- kolom device --}}
+                            <label class="mt-1" for="device"><h2>Device Name</h2></label>
+                            <input type='text' textarea name="device" class="form-control @error('device') is-invalid @enderror" id="device" placeholder="device""></input>
+                            @error('device')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
 
-                                {{-- kolom device --}}
-                                <label class="mt-2" for="device">Device Name</label>
-                                <input type='text' textarea name="device" class="form-control @error('device') is-invalid @enderror" id="device" placeholder="device""></input>
-                                @error('device')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                                @enderror
-
-                                {{-- tombol add data --}}
-                                <button class="btn btn-success mt-4">Add data</button>
-                            </form>
+                            {{-- tombol add data --}}
+                            <button class="btn btn-success mt-4">Add data</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
